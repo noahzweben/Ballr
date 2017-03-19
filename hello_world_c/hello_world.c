a1f4f009256d859f0e169838851871b7d6976bc2
jvandebon <jvandebon@gmail.com>
2017-03-19 13:33:42 -0400

    adding endzone and collision detection

--- hello_world_c/hello_world.c
+++ hello_world_c/hello_world.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "/Users/Jessie/Desktop/SDL_PLT/SDL/include/SDL.h"
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
@@ -48,6 +49,26 @@ void closeWindow()
 	SDL_Quit();
 }
 
+int inEndZone( SDL_Rect block, SDL_Rect endZone )
+{
+
+    int leftBlock = block.x;
+    int rightBlock = block.x + block.w;
+    int bottomBlock = block.y;
+    int topBlock = block.y + block.h;
+        
+    int leftEZ = endZone.x;
+    int rightEZ = endZone.x + endZone.w;
+    int bottomEZ = endZone.y;
+    int topEZ  = endZone.y + endZone.h;
+
+    if ( bottomBlock > bottomEZ && topBlock < topEZ && leftBlock > leftEZ && rightBlock < rightEZ ) {
+    	return 1;
+    }
+
+    return 0;
+}
+
 int main( int argc, char ** argv )
 {
 	
@@ -58,24 +79,34 @@ int main( int argc, char ** argv )
 
 	int quit = 0;
 	SDL_Event e;
-	SDL_Rect block = { 50, 50, SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4 };
-
+	SDL_Rect block = {10,SCREEN_HEIGHT / 2 - 10, 20, 20 };
+	SDL_Rect endZone = {SCREEN_WIDTH - 100, SCREEN_HEIGHT / 2 - 100, 100, 200 };
+	
+	SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+	SDL_UpdateWindowSurface( window );
 
 	while( !quit ) {
 
 		while ( SDL_PollEvent( &e ) != 0 ) {
-            
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+			
+
+			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+			SDL_FillRect( screenSurface, &endZone, SDL_MapRGB( screenSurface->format, 0x00, 0xFF, 0x00 ));
+			SDL_FillRect( screenSurface, &block, SDL_MapRGB( screenSurface->format, 0xFF, 0x00, 0x00 ));
 			SDL_UpdateWindowSurface( window );
 
-			SDL_FillRect( screenSurface, &block, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0xFF ));
-			SDL_UpdateWindowSurface( window );
+			if ( inEndZone( block, endZone ) ) {
+				block.x = 10;
+				block.y = SCREEN_HEIGHT / 2 - 10;
+				continue;
+			}
 
             if ( e.type == SDL_QUIT ) {
 
                 quit = 1;
 
-            } else if (e.type == SDL_KEYDOWN ){
+            } else if ( e.type == SDL_KEYDOWN ){
             	
             	switch( e.key.keysym.sym )
                 {
@@ -100,12 +131,7 @@ int main( int argc, char ** argv )
                     break;
                 }
             }
-        }
-
-       	SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-		SDL_FillRect( screenSurface, &block, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0xFF ));
-		SDL_UpdateWindowSurface( window ); 
-		
+        }		
 	}
 		
 	closeWindow();
