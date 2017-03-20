07211b5fc8f419eaaa61947eee7f69bf25df1b5a
Noah Zweben <noahzweben@gmail.com>
2017-03-20 16:53:10 -0400

    run script

---
+++ run.bat
@@ -0,0 +1,1 @@
+ocamllex scanner.mll && ocamlyacc parser.mly && ocamlc -c ast.mli && ocamlc -c parser.mli && ocamlc -c scanner.ml && ocamlc -c parser.ml && ocamlc -c ballr.ml && ocamlc -o ballr parser.cmo scanner.cmo ballr.cmo
