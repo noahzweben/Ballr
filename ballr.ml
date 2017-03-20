ec34282dba423414bfd1695e835b8de9a8e8c73e
Noah Zweben <noahzweben@gmail.com>
2017-03-20 17:21:23 -0400

    updated ballr

--- ballr.ml
+++ ballr.ml
@@ -1,10 +1,9 @@
 
-
 type action = Ast 
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-a", Ast);] (* Generate, check LLVM IR *)
+    List.assoc Sys.argv.(1) [ ("-a", Ast);]
   else Ast in
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.token lexbuf in
