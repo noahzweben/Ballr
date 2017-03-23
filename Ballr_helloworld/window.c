25d290822e2c90497400328a94d2995a7fe25e58
jvandebon <jvandebon@gmail.com>
2017-03-22 23:23:21 -0400

    setting up SDL and linking evrything in VM

--- Ballr_helloworld/window.c
+++ Ballr_helloworld/window.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include "SDL2/SDL.h"
-#include "/Users/Jessie/Desktop/SDL_PLT/SDL/include/SDL.h"
+#include "SDL2/SDL.h"
 #include "window.h"
 
 #define MS_PER_FRAME 17
