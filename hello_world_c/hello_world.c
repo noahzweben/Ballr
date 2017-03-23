06b65c52b07fa3ab69f73d61d068d96e3ea31d94
Frederick Kellison-Linn <fred.kl@me.com>
2017-03-23 11:50:58 -0400

    Make SDL path more general

--- hello_world_c/hello_world.c
+++ hello_world_c/hello_world.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "/Users/Jessie/Desktop/SDL_PLT/SDL/include/SDL.h"
+#include "SDL2/SDL.h"
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
