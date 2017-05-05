d2751e5c32b635a271dc62d489e7a92d27728b3c
Frederick Kellison-Linn <fjk2119@columbia.edu>
2017-05-05 19:40:39 -0400

    Update ballr.ml so that running ballr also compiles using clang, instead of just outputting LLVM IR

--- ballr.ml
+++ ballr.ml
@@ -18,14 +18,15 @@ let _ =
   else 
     Compile
   in
-  let (ic, oc) = 
+  let (ic, oc, exec_name) = 
   if Array.length Sys.argv > 2 then 
     let in_f = Sys.argv.(2) in
     let i = (try String.rindex in_f '.' with Not_found -> String.length in_f) in
-    let out_f = (String.sub in_f 0 i) ^ ".ll" in
-    (open_in in_f, open_out out_f)
+    let exec_name = String.sub in_f 0 i in
+    let out_f = exec_name ^ ".ll" in
+    (open_in in_f, open_out out_f, exec_name)
   else 
-    (stdin, stdout)
+    (stdin, stdout, "")
   in
   let lexbuf = Lexing.from_channel ic in
   let ast = Parser.program Scanner.token lexbuf in
@@ -35,4 +36,11 @@ let _ =
   | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen_simple.translate ast))
   | Compile -> let m = Codegen_simple.translate ast in
     Llvm_analysis.assert_valid_module m;
-    fprintf oc "%s" (Llvm.string_of_llmodule m)
+    fprintf oc "%s\n" (Llvm.string_of_llmodule m);
+    close_out oc;
+    if exec_name != "" then
+      let cmd = "clang -c " ^ exec_name ^ ".ll && clang -g " ^ exec_name ^ ".o -o " ^ exec_name ^ " -L/usr/local/lib -Wl,-rpath,/usr/local/lib -Wl,--enable-new-dtags -lSDL2 -L./runtime/build -lblr_rt" in
+      print_string (cmd ^ "\n");
+      ignore (Sys.command cmd)
+    else ()
+
