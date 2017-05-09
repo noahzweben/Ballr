086533f01721884596b766b362f2999d7ccc8af3
jvandebon <jvandebon@gmail.com>
2017-05-08 20:32:46 -0400

    added lots of 'good' tests, some issues with negativer numbers to be fixed

--- testall.sh
+++ testall.sh
@@ -76,7 +76,7 @@ Check() {
 
     generatedfiles=""
 
-    generatedfiles="$generatedfiles ${basename}.ll ${basename}.o ${basename} ${basename}.out" &&
+    generatedfiles="$generatedfiles tests/${basename}.ll ${basename}.o ${basename} ${basename}.out" &&
     Run "make ${basename}.test > /dev/null"
     Run "./${basename}" > "${basename}.out" 
     Compare ${basename}.out ${reffile}.out ${basename}.diff
@@ -95,7 +95,6 @@ Check() {
     fi
 }
 
-# NEED TO MODIFY THIS WHEN WE ADD ERROR MESSAGES
 CheckFail() {
     error=0
     basename=`echo $1 | sed 's/.*\\///
@@ -110,7 +109,7 @@ CheckFail() {
 
     generatedfiles=""
 
-    generatedfiles="$generatedfiles ${basename}.ll ${basename}.o ${basename} ${basename}.err" &&
+    generatedfiles="$generatedfiles tests/${basename}.ll ${basename}.o ${basename} ${basename}.err" &&
     RunFail "make ${basename}.test 2> ${basename}.err 1> /dev/null"
     Compare ${basename}.err ${reffile}.err ${basename}.diff
 
